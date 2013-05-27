e8c46fc29f1329481c0bc0499ebbe1ecd4baa48b
Jim Geovedi <jim.geovedi@gmail.com>
2013-05-27 20:43:47 +0700

    simplified

--- distance.py
+++ distance.py
@@ -8,30 +8,21 @@ def jaccard1(a, b):
     a, b = set(a), set(b)
     top = a.intersection(b)
     under = a.union(b)
-    return float(top) / under
-
-
-def jaccard2(a, b):
-    a, b = set(a), set(b)
-    c = a.intersection(b)
-    under = (len(a) + len(b) - len(c)
-    return float(len(c)) / under
-
-jaccard = jaccard1
+    return top / under
 
 
 def tversky(a, b, alpha=1.0, beta=1.0):
     a, b = set(a), set(b)
     c = a.intersection(b)
     under = len(c) + alpha * len(a - b) + beta * len(b - a)
-    return float(len(c)) / under
+    return len(c) / under
 
 
 def dice(a, b):
     a, b = set(a), set(b)
-    c = a.intersection(b)
+    top = len(a.intersection(b))
     under = len(a) + len(b)
-    return float(len(c)) / under
+    return top / under
 
 
 def mountford(a, b):
@@ -39,7 +30,7 @@ def mountford(a, b):
     c = a.intersection(b)
     top = 2.0 * len(c)
     under = 2.0 * len(a) * len(b) - (len(a) + len(b)) * len(c)
-    return float(top) / under
+    return top / under
 
 
 def sorensen(a, b):
@@ -47,5 +38,5 @@ def sorensen(a, b):
     c = a.intersection(b)
     top = 2.0 * len(c)
     under = len(a) + len(b)
-    return float(top) / under
+    return top / under
 
